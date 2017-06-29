34b0c9414a36d409030abf76727436c16a1fb96d
manojkumarzook <rmanojkumar23@gmail.com>
2017-06-29 14:16:32 +0530

    Create hate.c

---
+++ hate.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr;
+printf("enter the value");
+scanf("%d",&a);
+if(a>0)
+printf("positive");
+else
+printf("negative");
+getch();
+}
